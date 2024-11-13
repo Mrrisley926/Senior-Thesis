726d86019762bf314143fba3a19b8e635da02d24
Mrrisley926 <matthew.r.risley@gmail.com>
2024-11-13 12:16:42 -0800

    Importing Datasets

---
+++ Importing:Looking.R
@@ -0,0 +1,5 @@
+##Importing Data##
+
+VDEM_Electoral_Data <- read.csv("/Users/matthewrisley/Desktop/Senior Thesis /V-Dem-CY-FullOthers-v14_csv_YyKfizl/cleaned_VDEM_data.csv")
+
+head(VDEM_Electoral_Data)
